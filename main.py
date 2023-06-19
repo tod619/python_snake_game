33738a2853781b14b2adc81542a124dd16b8f988
tod619 <trevorodwyer619@hotmail.com>
2023-06-19 20:21:22 +0100

    create starting snake segements using for loop in main.py file

--- main.py
+++ main.py
@@ -4,9 +4,20 @@
 
 from turtle import Turtle, Screen
 
+# Screen set up
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+# Starting postions
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+# Creating the snake
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
 
 screen.exitonclick()
