7c696da740598a00b475964535c9034a9cc41bd6
tod619 <trevorodwyer619@hotmail.com>
2023-06-20 21:16:44 +0100

    fix spelling mistake + create extend func in snake.py + call in main.py file

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ while game_is_on:
     # Detect when snake collides with the food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     # Detect collision with the wall
