ce282e465172810236eb61d038083d5a721d0ba1
tod619 <trevorodwyer619@hotmail.com>
2023-06-20 21:04:13 +0100

    End game when snake collides with wall + display game over msg in main.py file

--- scoreboard.py
+++ scoreboard.py
@@ -22,3 +22,7 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
