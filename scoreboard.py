ead42c991de26da6364d1bce27817514197fd3bd
tod619 <trevorodwyer619@hotmail.com>
2023-06-23 20:53:05 +0100

    Delter game_over func + create reset func in scoreboard.py file

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -15,14 +16,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"SCORE: {self.score}", align=ALIGNMENT,
+        self.clear()
+        self.write(f"SCORE: {self.score} HIGH SCORE: {self.high_score}", align=ALIGNMENT,
                    font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+        self.score = 0
+        self.update_scoreboard()
