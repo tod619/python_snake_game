2821453c6181ed9db0374e089351d4dbc31aa23a
tod619 <trevorodwyer619@hotmail.com>
2023-06-23 20:55:38 +0100

    Call scoreboard reset func in main.py file

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ while game_is_on:
 
     # Detect collision with the wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        # game_is_on = False
+        scoreboard.reset()
 
     # Detect collision with the snake tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            # game_is_on = False
+            scoreboard.reset()
 
 
 screen.exitonclick()
