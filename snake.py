6618cad757efc661d20702c039d85a4911915146
tod619 <trevorodwyer619@hotmail.com>
2023-06-24 17:58:37 +0100

    Create data.txt file for the project

--- snake.py
+++ snake.py
@@ -31,6 +31,9 @@ class Snake():
         self.add_segment(self.segments[-1].position())
 
     def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+
         self.segments.clear()
         self.create_snake()
         self.head = self.segments[0]
